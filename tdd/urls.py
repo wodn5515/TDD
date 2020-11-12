c2927997e666ea16d5a4f85d80af62dfbe364e0b
wodn5515 <wodn5515@naver.com>
2020-11-12 15:41:02 +0900

    201112-3 : 첫 단위 테스트와 url 맵핑 그리고 임시 뷰

--- tdd/urls.py
+++ tdd/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from lists import views as list_views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', list_views.home_page, name='home')
+    # path('admin/', admin.site.urls),
 ]
