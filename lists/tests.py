d74a0d3eea7126ede381f2a4f9c0815ffdc98dc6
wodn5515 <wodn5515@naver.com>
2020-11-13 14:20:26 +0900

    201113-3 : 53p~71p

--- lists/tests.py
+++ lists/tests.py
@@ -3,18 +3,36 @@ from django.test import TestCase
 from django.template.loader import render_to_string
 from django.http import HttpRequest
 from lists.views import home_page
-
+import re
 
 # Create your tests here.
 class HomePageTest(TestCase):
+
+    # csrftoken input tag 제거
+    def remove_csrf(self, origin):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        re.sub(csrf_regex, '', origin)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
 
-
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'},
+            request=request
+        )
+        self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
